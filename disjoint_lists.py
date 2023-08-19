9f43d5ca6c70667276b53d9e8aa3ae53246ba1fe
Idan Kogan <56994399+Idank96@users.noreply.github.com>
2023-08-19 10:49:57 +0300

    Update disjoint_lists.py

--- disjoint_lists.py
+++ disjoint_lists.py
@@ -1,22 +1,28 @@
 from unsorted_list import UnsortedList
 
 
-# this class represents disjoint unsorted list.
-# using insert(), minimum(), extract_min() and union() from UnsortedList class.
-# functions: insert
 class DisjointLists(UnsortedList):
+    """
+    This class represents disjoint unsorted list.
+    Using insert(), minimum(), extract_min() and union() from UnsortedList class.
+    Functions: insert
+    """
     def __init__(self):
         super().__init__()
 
-    # make heap by calling the constructor
-    # time complexity: O(1)
     def make_heap(self):
+        """
+        Make heap by calling the constructor
+        Time complexity: O(1)
+        """
         return DisjointLists()
 
-    # if inserting to lst1 - no problem
-    # if inserting to lst2 - check if the value not exists in lst 1, then insert.
     @staticmethod
     def insert(l1, l2, first_heap, value):
+        """
+        If inserting to lst1 - no problem
+        If inserting to lst2 - check if the value not exists in lst 1, then insert.
+        """
         if first_heap:
             super(DisjointLists, l1).insert(value)
         else:
